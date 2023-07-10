de7d10914a2a63421f8c7d7c2366b166885d66c5
Tetiana Beznosikova <beznosikovat@gmail.com>
2023-07-10 09:41:38 +0200

    Add EasyAdminBundle bundle

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Option;
+use App\Entity\Question;
+use App\Entity\Survey;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+
+class DashboardController extends AbstractDashboardController
+{
+
+    public function index(): Response
+    {
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(SurveyCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Surveys list');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'web.survey.index');
+        yield MenuItem::linkToCrud('Surveys', 'fas fa-list', Survey::class);
+        yield MenuItem::linkToCrud('Questions', 'fas fa-list', Question::class);
+        yield MenuItem::linkToCrud('Options', 'fas fa-list', Option::class);
+    }
+}
